4ca5d43e424cdd354337cde2d81035eb1fd820be
Argho Maitra <187108030+ohgrmait@users.noreply.github.com>
2025-01-06 13:14:15 +0530

    Add functionality to display and guess the letters

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,9 @@
 class Game
   def initialize
     @word = nil
+    @guess = nil
     @board = nil
+    @player = Human.new
     @host = Computer.new
   end
 
@@ -10,7 +12,19 @@ class Game
     @word
   end
 
+  def guess_letter
+    @guess = @player.guess_letter
+    @guess
+  end
+
   def create_board
     @board = Array.new(@word.length, '_')
+  end
+
+  def current_display
+    @board.each do |elem|
+      print "#{elem} "
+    end
+    puts ''
   end
 end
--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,4 +1,5 @@
 require_relative 'game'
+require_relative 'human'
 require_relative 'computer'
 
 class Hangman
@@ -13,7 +14,21 @@ class Hangman
   def play_game
     secret_word = @game.create_word
     puts "secret word: #{secret_word}"
+
+    puts ''
+
     @game.create_board
+    loop do
+      print 'your current word: '
+      @game.current_display
+
+      puts ''
+
+      print 'guess the letters: '
+      @game.guess_letter
+
+      puts ''
+    end
   end
 end
 
