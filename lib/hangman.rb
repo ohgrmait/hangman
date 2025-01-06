9fc71cd26e936bd067ef28772ddc3865a90e6ccf
Argho Maitra <187108030+ohgrmait@users.noreply.github.com>
2025-01-07 00:47:23 +0530

    Show display message when the program is executed

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -8,7 +8,19 @@ class Hangman
   end
 
   def start_game
+    show_prompt
     play_game
+  end
+
+  def show_prompt
+    puts '...A game of Hangman between two players: Human and Computer!'
+    puts ''
+    puts "...You can guess the opponent's secret word until two things:"
+    puts '   (1) until the stick figure is completely hung or (2) total'
+    puts '   of 8 incorrect guesses have been made by the player. It is'
+    puts '   a word guessing game where the player will have the option'
+    puts '   to both save the games at every turn and load a saved game'
+    puts '   at the beginning of each turn when the program loads.'
   end
 
   def play_game
